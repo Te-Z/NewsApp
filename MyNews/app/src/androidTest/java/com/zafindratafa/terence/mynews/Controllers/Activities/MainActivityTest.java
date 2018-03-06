6116434739470f5e9cdc93dcc9c524feab792f48
Tez <t.zafindratafa@gmail.com>
2018-03-06 16:53:35 +0100

    Codecov.io added

---
+++ MyNews/app/src/androidTest/java/com/zafindratafa/terence/mynews/Controllers/Activities/MainActivityTest.java
@@ -0,0 +1,50 @@
+package com.zafindratafa.terence.mynews.Controllers.Activities;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.zafindratafa.terence.mynews.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.PositionAssertions.isCompletelyAbove;
+import static android.support.test.espresso.assertion.PositionAssertions.isPartiallyAbove;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by maverick on 06/03/18.
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mMainActivityActivityTestRule =
+            new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    // Test if the activity is created
+    @Test
+    public void creationTest() throws Exception {
+        onView(withId(R.id.activity_main_drawer_layout)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void toolBarDisplayTest() throws Exception {
+        onView(withId(R.id.activity_main_toolbar)).check(matches(isDisplayed()));
+    }
+
+    /*// Test position of elements
+    @Test
+    public void toolbarPositionTest() throws Exception {
+        onView(withId(R.id.activity_main_toolbar)).check(isCompletelyAbove(withId(R.id.activity_main_tabs)));
+    }
+
+    @Test
+    public void tabsPositionTest() throws Exception {
+        onView(withId(R.id.activity_main_tabs)).check(isPartiallyAbove(R.id.activity_main_viewpager));
+    }*/
+}
---
+++ MyNews/app/src/main/java/com/zafindratafa/terence/mynews/Controllers/Activities/MainActivity.java
@@ -0,0 +1,40 @@
+package com.zafindratafa.terence.mynews.Controllers.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.zafindratafa.terence.mynews.R;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_main_toolbar)
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu and add it to the Toolbar
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+    // ---------------
+    // CONFIGURATION
+    // ---------------
+
+    private void configureToolbar(){
+        // Get the toolbar view inside the activity layout
+        setSupportActionBar(mToolbar);
+    }
+}
